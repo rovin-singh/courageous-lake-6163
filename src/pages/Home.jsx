fb01e4794dda206d6c557fe4d62f1c0d5d7d88bb
rovin76 <rovin7678>
2022-07-19 13:01:14 +0530

    addeded

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import {Container} from "@chakra-ui/react"
+import { Button } from 'react-bootstrap';
 const Home = () => {
   return (
     <div>
-      <Container>
+        <Button  variant="primary">Add</Button>
+      <p>
         There are many benefits to a joint design and development system. Not
         only does it bring benefits to the design team, but it also brings
         benefits to engineering teams. It makes sure that our experiences have a
         consistent look and feel, not just in our design specs, but in
         production
-      </Container>
+      </p>
     </div>
   );
 };
