ef1cba9b622029831ba8f0e458e57b9122697358
rovin76 <rovin7678>
2022-07-19 13:23:11 +0530

    addd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,7 @@ const Home = () => {
         There are many benefits to a joint design and development system. Not
         only does it bring benefits to the design team, but it also brings
         benefits to engineering teams. It makes sure that our experiences have a
-        consistent look and feel, not just in our design specs, but in
-        production
+        cons
       </p>
     </div>
   );
